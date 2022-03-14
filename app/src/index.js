d1dbd884407cb39087a5e5304d07fdf931bcd3a0
haahannah <nguyenhaanh96218@gmail.com>
2022-03-14 13:01:01 +0100

    js structure and code cleanup

--- app/src/index.js
+++ app/src/index.js
@@ -1,5 +1,1 @@
-function init() {
-    console.log("### Starting MME Project ###");
-}
-
-init();
+!function(){"use strict";var e={r:function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})}};e.r({})}(); 
