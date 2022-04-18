41c6280e6f956d879bafa44468860c28f85dc3f4
Myanhnguyenphan <89725361+Myanhnguyenphan@users.noreply.github.com>
2022-04-18 22:35:42 +0200

    cleanup

--- app/src/assets/src/js/login.js
+++ app/src/assets/src/js/login.js
@@ -1,68 +1,67 @@
-/* eslint-disable */
 const signup = async ({ email, password, name }) => {
-  try {
-    const account = await api.createAccount(email, password, name);
-    await api.createSession(email, password);
-    await api.createDocument(Server.profileCollectionId, {
-      user_id: account.$id,
-      user_name: name,
-      user_email: account.email
-    });
-    document.location.href = "chat.html";
-  } catch (e) {
-    alert(e);
-  }
-};
+	try {
+		const account = await api.createAccount(email, password, name)
+		await api.createSession(email, password)
+		await api.createDocument(Server.profileCollectionId, {
+			user_id: account.$id,
+			user_name: name,
+			user_email: account.email
+		})
+    
+		document.location.href = "chat.html"
+	} catch (e) {
+		alert(e)
+	}
+}
 
 const fetchAccount = async () => {
-  try {
-    const account = await api.getAccount();
-  } catch (e) {
-    console.log(e);
-    document.location.href = "login.html";
-  }
-};
+	try {
+		const account = await api.getAccount()
+	} catch (e) {
+		document.location.href = "login.html"
+	}
+}
 
 const login = async ({ email, password }) => {
-  try {
-    await api.createSession(email, password);
-    const account = await api.getAccount();
-    document.location.href = "chat.html";
-  } catch (e) {
-    alert(e);
-  }
-};
+	try {
+		await api.createSession(email, password)
+		const account = await api.getAccount()
+		document.location.href = "chat.html"
+	} catch (e) {
+		alert(e)
+	}
+}
 
 const logout = async () => {
-  try {
-    await api.deleteCurrentSession();
-    document.location.href = "login.html";
-  } catch (e) {
-    alert(e);
-  }
-};
+	try {
+		await api.deleteCurrentSession()
+		document.location.href = "login.html"
+	} catch (e) {
+		alert(e)
+	}
+}
 
 function login_with_appwrite() {
-  const email = document.getElementById("login_email").value;
-  const password = document.getElementById("login_password").value;
+	const email = document.getElementById("login_email").value
+	const password = document.getElementById("login_password").value
 
-  login({ email, password });
+	login({ email, password })
 }
 
 function signup_with_appwrite() {
-  const name = document.getElementById("signup_name").value;
-  const email = document.getElementById("signup_email").value;
-  const password = document.getElementById("signup_password").value;
+	const name = document.getElementById("signup_name").value
+	const email = document.getElementById("signup_email").value
+	const password = document.getElementById("signup_password").value
 
-  signup({ email, password, name });
+	signup({ email, password, name })
 }
 
 function redirect_to_login() {
-  $(".login-form").show();
-  $(".signup-form").hide();
+	$(".login-form").show()
+	$(".signup-form").hide()
 }
 
 function redirect_to_signup() {
-  $(".login-form").hide();
-  $(".signup-form").show();
+	$(".login-form").hide()
+	$(".signup-form").show()
 }
